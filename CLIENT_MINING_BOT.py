a3cb3e2437ef650abdd5adb7eb2b7c4c0a571c24
music-rusher02 <100468239+music-rusher02@users.noreply.github.com>
2022-02-26 14:15:42 +0300

    Update CLIENT_MINING_BOT.py

--- CLIENT_MINING_BOT.py
+++ CLIENT_MINING_BOT.py
@@ -2,7 +2,8 @@ import socket
 import time as t
 connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 IP = input("Введите IP адрес вашего устройства: ")
-PORT = input("Введите PORT вашего устройства: ")
+port = input("Введите PORT вашего устройства: ")
+PORT = int(port)
 connection.connect((IP, PORT))
 rd = connection.recv(1024)
 print(rd.decode('utf8'))
