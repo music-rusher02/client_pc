089bf19493bc0ee3888b543b3152704bfd2d6ec5
music-rusher02 <100468239+music-rusher02@users.noreply.github.com>
2022-02-26 13:06:48 +0300

    Add files via upload

---
+++ CLIENT_MINING_BOT.py
@@ -0,0 +1,12 @@
+import socket
+import time as t
+connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+IP = socket.gethostbyname(socket.gethostname())
+PORT = 12333
+connection.connect((IP, PORT))
+rd = connection.recv(1024)
+print(rd.decode('utf8'))
+while True:
+	connection.send("И тебе привет!".encode('utf8'))
+	t.sleep(2)
+#connection.close()
